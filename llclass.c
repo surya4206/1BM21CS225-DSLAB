a97d16f88d4e973be4d9c844f4e6b5b312777065
surya4206 <117361908+surya4206@users.noreply.github.com>
2022-12-17 11:12:35 +0530

    Update llclass.c

--- llclass.c
+++ llclass.c
@@ -6,19 +6,21 @@ struct NODE
     struct NODE *link;
 };
 typedef struct NODE node;
+node *start;
 void create()
 {
-    char ch;
-    node *start,*new,*curr;
+    int ch;
+    node *new,*curr=NULL;
     start=NULL;
-    new=(node *)malloc(sizeof(node));
+    start=(node *)malloc(sizeof(node));
+    curr=start;
     printf("Enter the element:");
     scanf("%d",&start->data);
     while(1)
     {
-        printf("Add another element?");
-        scanf("%c",&ch);
-        if(ch=='y'||ch=='Y')
+        printf("Add another element (1 for yes 0 for no)?");
+        scanf("%d",&ch);
+        if(ch)
         {
             new=(node *)malloc(sizeof(node));
             printf("Enter the element:");
@@ -32,6 +34,69 @@ void create()
             break;
         }
     }
+}
+void insert_beg()
+{
+    node *new;
+    new=(node *)malloc(sizeof(node));
+    printf("\nEnter element:");
+    scanf("%d",&new->data);
+    if(start==NULL)
+    {
+        start=new;
+        new->link=NULL;
+        return;
+    }
+    new->link=start;
+    start=new;
+}
+void insert_end()
+{
+    node *new,*temp;
+    new=(node *)malloc(sizeof(node));
+    printf("\nEnter element:");
+    scanf("%d",&new->data);
+    if(start==NULL)
+    {
+        start=new;
+        new->link=NULL;
+        return;
+    }
+    temp=start;
+    while(temp->link!=NULL)
+        temp=temp->link;
+    temp->link=new;
+    new->link=NULL;
+}
+void insert_pos()
+{
+    node *new,*temp;
+    int pos,i=0;
+    new=(node *)malloc(sizeof(node));
+    printf("\nEnter element:");
+    scanf("%d",&new->data);
+    printf("\nEnter position:");
+    scanf("%d",&pos);
+    if(pos==1)
+    {
+        new->link=start;
+        start=new;
+        return;
+    }
+    temp=start;
+    while(i<(pos-1)&&temp->link!=NULL)
+    {
+        temp=temp->link;
+        i++;
+    }
+    if(i==(pos-1))
+    {
+        new->link=temp->link;
+        temp->link=new;
+        return;
+    }
+    if(temp==NULL)
+        printf("\nInvalid Position");
 }
 void display()
 {
@@ -50,6 +115,27 @@ void display()
 }
 void main()
 {
-    create();
-    display();
+    int choice;
+    while(1)
+    {
+        printf("1.Create 2.Insert at the beginning 3.Insert at the end 4.Insert at a given position 5.Display 6.Exit\n");
+        printf("Enter your choice:");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:create();
+                   break;
+            case 2:insert_beg();
+                   break;
+            case 3:insert_end();
+                   break;
+            case 4:insert_pos();
+                   break;
+            case 5:display();
+                   break;
+            case 6:exit(0);
+                   break;
+            default:printf("Wrong choice\n");
+        }
+    }
 }
